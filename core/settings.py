6efa6414c39a9dd5c91f1d5130ac5773529431f7
EC2 Default User <ec2-user@ip-172-31-34-11.eu-west-1.compute.internal>
2024-03-26 10:58:31 +0000

    added deployment files

--- core/settings.py
+++ core/settings.py
@@ -14,21 +14,22 @@ from pathlib import Path
 import os
 from django.contrib.messages import constants as messages
 from django.urls import reverse_lazy
-import environ
+# import environ
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-env = environ.Env()
-environ.Env.read_env()
+# env = environ.Env()
+# environ.Env.read_env()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = env("SECRET_KEY")
+# SECRET_KEY = env("SECRET_KEY")
+SECRET_KEY="django-insecure-==64$+0xzy0$w_!7qgi39@1pkli*ejx3p%vf%y7^r54oblefdw"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
